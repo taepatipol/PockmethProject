0b10b24fa4a1814802e8c97cd657e37a4c9b9e8c
Patipol Wuthiprasert <abcteapatipol@gmail.com>
2017-11-16 14:50:23 +0700

    EnviBox created

--- src/model/EnvironmentBox.java
+++ src/model/EnvironmentBox.java
@@ -1,9 +1,28 @@
 package model;
 
 public class EnvironmentBox {
-	boolean RWall;
-	boolean DWall;
-	public EnvironmentBox(boolean RWall,boolean DWall) {
-		
+	private boolean UWall;
+	private boolean DWall;
+	private boolean LWall;
+	private boolean RWall;
+	public EnvironmentBox(boolean UWall,boolean DWall,boolean LWall, boolean RWall) {
+		this.UWall = UWall;
+		this.DWall = DWall;
+		this.LWall = LWall;
+		this.RWall = RWall;
 	}
+	public boolean hasUWall() {
+		return UWall;
+	}
+	public boolean hasDWall() {
+		return DWall;
+	}
+	public boolean hasLWall() {
+		return LWall;
+	}
+	public boolean hasRWall() {
+		return RWall;
+	}
+	
+	
 }
