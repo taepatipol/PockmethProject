5dad86ed10253f1524becda4743423682e19e2f4
Phongpak Taothong <phongpak2925809@gmail.com>
2017-11-16 13:48:38 +0700

    Smart!

--- src/model/SmartEnemy.java
+++ src/model/SmartEnemy.java
@@ -1,5 +1,14 @@
 package model;
 
 public class SmartEnemy {
-
+	private boolean isSmart;
+	public SmartEnemy() { //set enemy with 2x speed and random location
+		super();
+		this.isSmart = true;
+	}
+	
+	
+	
+	
+		
 }
